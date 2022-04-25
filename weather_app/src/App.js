21e1f8b135fd493bf4fdcebfffd6f37b71d98211
Mohit kumar <64029983+kmohitroy@users.noreply.github.com>
2022-04-25 22:21:09 +0530

    Add files via upload

---
+++ weather_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import City from './components/City'
+import Weather from './components/Weather'
+
+function App() {
+  return (
+   <>
+    <City/>
+    {/* <Weather/> */}
+   </>
+  );
+}
+
+export default App;
